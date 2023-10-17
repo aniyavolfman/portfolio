3e0536c5c50fea676aaef1d554369e2239840e10
Anna Volfman <aniya_07@ukr.net>
2023-10-17 16:26:05 +0300

    update portfolio

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function getTheme() {
     footer.classList.add(savedTheme);
   }
   if (body.classList.contains('dark-mode')) {
-    themeToggle.textContent = 'Light mode';
+    themeToggle.textContent = 'Bright mode';
   } else {
     themeToggle.textContent = 'Dark mode';
   }
