9d812f6800d19e6b816ca5237afc3c99a7319958
Anna Volfman <aniya_07@ukr.net>
2023-10-17 15:02:14 +0300

    update portfolio

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ const moreText2 = document.querySelector('.more-text2');
 const buttonText2 = document.querySelector('#show-more2');
 const moreText3 = document.querySelector('.more-text3');
 const buttonText3 = document.querySelector('#show-more3');
+const body = document.body;
+const hero = document.querySelector('.main__container');
+const portfolioList = document.querySelector('.portfolio__list');
+const anchor = document.querySelector('.anchor');
+const footer = document.querySelector('.footer_container');
+const themeToggle = document.querySelector('#themeToggle');
+
+const savedTheme = localStorage.getItem('theme');
+
+getTheme();
+
+function getTheme() {
+  if (savedTheme) {
+    document.body.classList.add(savedTheme);
+    hero.classList.add(savedTheme);
+    portfolioList.classList.add(savedTheme);
+    anchor.classList.add(savedTheme);
+    footer.classList.add(savedTheme);
+  }
+  if (body.classList.contains('dark-mode')) {
+    themeToggle.textContent = 'Light mode';
+  } else {
+    themeToggle.textContent = 'Dark mode';
+  }
+}
 
 function toggleText() {
   if (moreText.style.display === 'none' || moreText.style.display === '') {
@@ -32,5 +57,22 @@ function toggleText3() {
   } else {
     moreText3.style.display = 'none';
     buttonText3.innerText = 'Read more';
+  }
+}
+
+function toggleTheme() {
+  const theme = body.classList.contains('dark-mode') ? '' : 'dark-mode';
+  localStorage.setItem('theme', theme);
+
+  body.classList.toggle('dark-mode');
+  hero.classList.toggle('dark-mode');
+  portfolioList.classList.toggle('dark-mode');
+  anchor.classList.toggle('dark-mode');
+  footer.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    themeToggle.textContent = 'Light mode';
+  } else {
+    themeToggle.textContent = 'Dark mode';
   }
 }
